907157bde972ce65a3bdfb80cfd45ee976de84cd
Tommy <Michael.t.coleman7@gmail.com>
2023-04-12 16:06:22 -0700

    broken

--- deployCommands.mjs
+++ deployCommands.mjs
@@ -4,38 +4,40 @@ import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-dotenv.config(); //load environment variables
-const __filename = fileURLToPath(import.meta.url); //get file path to this file
-const __dirname = path.dirname(__filename); //use file path to get files directory
+//expose environment variables
+dotenv.config();
+
+//get current file path and directory
+const __filename = import.meta.url;
+const __dirname = path.dirname(__filename);
 
 //grab all command files from the commands directory
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".mjs"));
 
-const myPromise = new Promise((resolve, reject) => {
-  const commands = [];
-  for (const file of commandFiles) {
-    const filePath = `file:\\\\\\` + path.join(commandsPath, file);
-    import(filePath)
-      .then((command) => {
-        // console.log(command.data.toJSON());
-        commands.push(command.data.toJSON());
-        //commands is empty!!
-      })
-      .catch((error) => console.log(`error at ${filePath}: ${error}`));
-  }
-  resolve(commands);
-});
+//create an array of import statements that the promise.all method can call.
+const importCommands = commandFiles.map((commandFile) => {});
 
-myPromise.then((value) => {
-  console.log(value);
-});
+// for (const file of commandFiles) {
+//   console.log(file);
+
+//   const filePath = `file:\\\\\\` + path.join(commandsPath, file);
+//   console.log(filePath);
+//     import(filePath)
+//       .then((command) => {
+//         // console.log(command.data.toJSON());
+//         commands.push(command.data.toJSON());
+//         //commands is empty!! WTF. async problem
+//       })
+//       .catch((error) => console.log(`error at ${filePath}: ${error}`));
+// }
 
 //construct an instance of the rest module
 // const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 //deploy commands
 // (async () => {
+//   console.log(commands);
 //   try {
 //     console.log(`started refreshing ${commands.length} application (/) commands.`);
 //     console.log(`commands: ${commands}`);
